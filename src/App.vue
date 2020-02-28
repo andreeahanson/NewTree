f3f27a3173805db8c5d458038e6eb1cbddeda95c
Andreea Hanson <andreea.hanson7@gmail.com>
2020-02-27 23:12:28 -0700

    Add CalorieForm component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <CalorieForm /> 
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import CalorieForm from './components/CalorieForm/CalorieForm';
 import { example } from './apiCalls.js'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld,
+    CalorieForm
   },
     async created() {
     try {
---
+++ src/components/CalorieForm/CalorieForm.vue
@@ -0,0 +1,24 @@
+<template>
+  <form>
+    <input type="text" placeholder="Add ingredient" name="ingredient" />
+    <input type="number" placeholder="Add quantity" name="quantity" />
+    <input type="select" placeholder="unit of measure" name="unit" />
+    <button>Calculate</button>
+  </form>
+</template>
+
+<script>
+export default {
+  name: "CalorieForm"
+};
+</script>
+
+<style scoped>
+input {
+  background: greenyellow;
+  width: 100px;
+}
+button {
+  width: 60px;
+}
+</style>
